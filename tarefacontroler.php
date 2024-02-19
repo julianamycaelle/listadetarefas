01fa03c8a607d30368625a42d6ad7a7b33016c1a
julianamycaelle <82950865+julianamycaelle@users.noreply.github.com>
2024-02-19 17:19:14 -0300

    Update tarefacontroler.php

--- tarefacontroler.php
+++ tarefacontroler.php
@@ -4,28 +4,42 @@
 	require "tarefa.service.php";
 	require "conexao.php";
 
-	//echo '<pre>';
-	//print_r($_POST);
-	//echo '</pre>';
-   $acao = isset($_GET['acao']) ? $_GET['acao'] :$acao;
-    if(isset($_GET['acao'])&& $_GET['acao']=='inserir'){
-	$tarefa = new Tarefa();
-	$tarefa->__set('tarefa', $_POST['tarefa']);
 
-	$conexao = new Conexao();
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-	$tarefaService = new TarefaService($conexao, $tarefa);
-	$tarefaService->inserir();
+	if($acao == 'inserir' ) {
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
 
-	//echo '<pre>';
-	//print_r($tarefaService);
-	//echo '</pre>';
+		$conexao = new Conexao();
 
-    header('Location:nova_tarefa.php?inclusao=1');
-}else if ($acao=='recuperar'){
-   $tarefa= new Tarefa();
-   $conexao= new Conexao();
-   $tarefaService=new TarefaService($conexao, $tarefa);
-   $tarefas = $tarefaService->recuperar();
-}
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
+
+		header('Location: nova_tarefa.php?inclusao=1');
+	
+	} else if($acao == 'recuperar') {
+		
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperar();
+	
+	} else if($acao == 'atualizar') {
+
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_POST['id']);
+		$tarefa->__set('tarefa', $_POST['tarefa']);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		if($tarefaService->atualizar()) {
+			header('location: todas_tarefas.php');
+		}
+
+
+	}
+
 ?>
